1190b92b5d2e74871a8637865e2cf76dd8586d85
cg1101 <cg1101@gmail.com>
2018-02-11 17:07:28 +1100

    added toggle in top-nav

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'op-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss']
+  styleUrls: ['./top-nav.component.scss'],
+  providers: [ NgClass ]
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+  private infoStub: {
+    notification: boolean,
+    bookmark: boolean,
+    history: boolean,
+  };
+
+  constructor(private ngClass: NgClass) {
+    this.infoStub = {
+      notification: false,
+      bookmark: false,
+      history: false,
+    }
+  }
 
   ngOnInit() {
   }
 
+  toggleInfo(key: string) : void {
+    var showInfo = false;
+    if (this.infoStub[key]) {
+      this.infoStub.notification = false;
+      this.infoStub.bookmark = false;
+      this.infoStub.history = false;
+    } else {
+      this.infoStub.notification = false;
+      this.infoStub.bookmark = false;
+      this.infoStub.history = false;
+      this.infoStub[key] = true;
+      showInfo = true;
+    }
+    this.ngClass.ngClass = showInfo ? 'info info-' + key : '';
+    this.ngClass.ngDoCheck();
+  }
 }
