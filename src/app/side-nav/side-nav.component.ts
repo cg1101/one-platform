156b1f95c1e354c028accaf7aae72ff3f9ff76f1
cg1101 <cg1101@gmail.com>
2018-02-11 11:57:36 +1100

    strip off nav.navigation from side-nav, use side-nav host itself to carry CSS class 'wide', wrap top-nav and router-outlet inside div#main which adjusts margin-left automatically

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, Self } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'op-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
+  providers: [ NgClass ]
 })
 export class SideNavComponent implements OnInit {
 
   wide: boolean;
-  constructor() {
+
+  constructor(@Self() private ngClass: NgClass) {
     this.wide = true;
   }
 
   ngOnInit() {
+  }
+
+  @HostBinding('class.wide') isWideOn() {
+    return this.wide;
+  }
+
+  toggleLabel() {
+    this.wide = !this.wide;
+    this.ngClass.ngClass = {wide: this.wide};
+    this.ngClass.ngDoCheck();
   }
 
 }
